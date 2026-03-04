f687b04fcf8f2427fa3132f2fdfea142f46230a3
tranlegiabao.com <tbao2446@gmail.com>
2026-03-04 14:09:22 +0700

    sum (a,b)

---
+++ main.py
@@ -0,0 +1,10 @@
+# calculate sum of two numbers
+
+def sum(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = 5
+    num2 = 10
+    result = sum(num1, num2)
+    print(f"The sum of {num1} and {num2} is: {result}")
