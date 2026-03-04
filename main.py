69667f74c9fda2205372c8d2533c83f8792e90ee
tranlegiabao.com <tbao2446@gmail.com>
2026-03-04 14:23:55 +0700

    update homework task1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def sum(a, b):
     return a + b
 
 if __name__ == "__main__":
-    num1 = 5
+    num1 = 20
     num2 = 10
     result = sum(num1, num2)
     print(f"The sum of {num1} and {num2} is: {result}")
